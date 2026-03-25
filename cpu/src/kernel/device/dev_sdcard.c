6c45d4911bbc5f0b8320a39eeda08a2303756b61
enorrmann <enorrmann@gmail.com>
2026-03-25 17:22:01 -0300

    comment

--- cpu/src/kernel/device/dev_sdcard.c
+++ cpu/src/kernel/device/dev_sdcard.c
@@ -46,6 +46,7 @@ under the terms of the GNU Affero General Public License as published by
 
 
 /*----- Macros -------------------------------------------------------*/
+// for some reason if I dont define this, app will crash
 #define DEBUG_SDDRIVER
 #ifdef DEBUG_SDDRIVER
 #   define DEBUG_LOG_SD(fmt, ...)  DEBUG_LOG(fmt, ##__VA_ARGS__)
