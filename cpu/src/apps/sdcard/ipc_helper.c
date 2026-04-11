6aeb90359afda4c53a9ade516abd879aa84315af
enorrmann <enorrmann@gmail.com>
2026-04-11 20:38:17 -0300

    variable naming

--- cpu/src/apps/sdcard/ipc_helper.c
+++ cpu/src/apps/sdcard/ipc_helper.c
@@ -7,11 +7,11 @@ uint32_t IPC_CHUNK_TRANSFER_SIZE =
                // word count in metadata)
 #define IPC_BUFFER_SIZE (1024 * 512)
 uint32_t ipc_buffer[IPC_BUFFER_SIZE];
-uint32_t ipc_buffer_index = 0; // Index for current position in ipc_buffer
+uint32_t total_samples = 0; // Index for current position in ipc_buffer
 const uint32_t initial_base_address = 0x00000060;
 
 void ipc_init_buffer() { 
-    ipc_buffer_index = 0;
+    total_samples = 0;
     // zero out the buffer
     uint32_t i;
     for (i = 0; i < IPC_BUFFER_SIZE; i++) {
@@ -36,11 +36,11 @@ void ipc_simple_send(uint32_t value) {
 }
 
 void ipc_add_to_buffer(uint32_t sample) {
-    ipc_buffer[ipc_buffer_index++] = sample;
+    ipc_buffer[total_samples++] = sample;
 }
 
 void ipc_send_buffer_via_param() {
-    uint32_t total_samples = ipc_buffer_index; 
+    
     uint32_t i;
     ft_set_module_param(0, PARAM_TRANSMISSION_START, 0);
     for (i = 0; i < total_samples; i++) {
@@ -51,23 +51,23 @@ void ipc_send_buffer_via_param() {
 }
 
 void ipc_send_buffer_chunked() {
-uint32_t total_samples = ipc_buffer_index; // total number of samples currently in the buffer    total_samples_read
+
 ft_set_module_param( 0, PARAM_TRANSMISSION_END, total_samples); // send total samples to dsp for playback before sending chunks to avoid delay and may cause glitche but is faster
     
 
-    uint32_t num_chunks = (total_samples + IPC_CHUNK_TRANSFER_SIZE - 1) / IPC_CHUNK_TRANSFER_SIZE;
-    DEBUG_LOG("Total samples: %i, num_chunks: %i", (int)total_samples, (int)num_chunks);
+    uint32_t total_chunks = (total_samples + IPC_CHUNK_TRANSFER_SIZE - 1) / IPC_CHUNK_TRANSFER_SIZE;
+    DEBUG_LOG("Total samples: %i, num_chunks: %i", (int)total_samples, (int)total_chunks);
     uint32_t base_address = initial_base_address;
-    for (uint32_t i = 0; i < num_chunks; i++) {
+    for (uint32_t chunk_number = 0; chunk_number < total_chunks; chunk_number++) {
 
-        uint32_t offset = (uint32_t)i * (uint32_t)IPC_CHUNK_TRANSFER_SIZE;
+        uint32_t offset = (uint32_t)chunk_number * (uint32_t)IPC_CHUNK_TRANSFER_SIZE;
 
         int status = dev_dsp_ipc_transfer(
             base_address, &ipc_buffer[offset], IPC_CHUNK_TRANSFER_SIZE, ipc_callback,
             (void *)0x23AC1D23 // arbitrary user context value for testing
         );
 
-         DEBUG_LOG("status: %d, chunk %u, count: %u, offset: %u, address: 0x%08x", status, i, IPC_CHUNK_TRANSFER_SIZE, offset, base_address);
+         DEBUG_LOG("status: %d, chunk %u, count: %u, offset: %u, address: 0x%08x", status, chunk_number, IPC_CHUNK_TRANSFER_SIZE, offset, base_address);
 
         base_address += (uint32_t)IPC_CHUNK_TRANSFER_SIZE * sizeof(int32_t);
     }
